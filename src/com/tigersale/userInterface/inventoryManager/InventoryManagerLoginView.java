6b4a1389d187c624f244a35eed7724449f7aea7d
James Haller <JamesHaller27@gmail.com>
2017-04-23 19:57:25 -0400

    Started InventoryManager ui

---
+++ src/com/tigersale/userInterface/inventoryManager/InventoryManagerLoginView.java
@@ -0,0 +1,75 @@
+package com.tigersale.userInterface.inventoryManager;
+
+import com.sun.corba.se.impl.io.TypeMismatchException;
+import com.tigersale.database.InventoryManagerTable;
+import com.tigersale.model.InventoryManager;
+import com.tigersale.userInterface.AbstractView;
+
+import java.sql.Date;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * @author James Haller
+ */
+public class InventoryManagerLoginView extends AbstractView {
+
+    public InventoryManagerLoginView(Scanner scanner) {
+        super(scanner);
+    }
+
+    /**
+     * This method acts as the view for the InventoryManager Login screen
+     */
+    public void runInventoryManagerLoginView() {
+        int choice = 0;
+
+        while(true)
+        {
+
+            System.out.println("Please choose from the following options (Enter the number corresponding to your choice):");
+            System.out.println("0: Go Back");
+            System.out.println("1: Login");
+
+            // Try to get a numeric response from the user
+            try {
+                choice = scanner.nextInt();
+            }
+            catch(InputMismatchException e)
+            {
+                System.out.println("Enter an integer corresponding to your preferred option.");
+                scanner.next();
+                continue;
+            }
+
+            switch (choice)
+            {
+                // Exit this view
+                case 0:
+                    return;
+                // Login
+                case 1:
+                    System.out.println("Please enter your username.");
+                    String username = scanner.next();
+                    System.out.println("Please enter your password.");
+                    String password = scanner.next();
+                    InventoryManager user = InventoryManagerTable.login(username, password);
+                    if(user == null)
+                    {
+                        System.out.println("Sorry, that username-password combination does not exist. Please try again.");
+                    }
+                    else
+                    {
+                        // Go to CustomerUserHomeView
+                        System.out.println("You have successfully logged in.");
+                        //(new InventoryManagerHomeView(scanner, user)).runCustomerUserHomeView();
+                    }
+                    break;
+                default:
+                    System.out.println("I am sorry, the option you chose does not exist. Please try again.");
+                    break;
+
+            }
+        }
+    }
+}
